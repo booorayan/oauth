467a2a3f91d8ddea607472bcbc1043b20fdd9e50
brian <bryantlch@gmail.com>
2023-09-15 15:01:47 +0300

    api authentication with oidc

---
+++ api/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token
+from .views import *
+
+
+urlpatterns = [
+    path('customers/', CustomerListCreateView.as_view(), name='customers'),
+    path('customers/<int:pk>/', CustomerDetailView.as_view(), name='customer-detail'),
+    path('orders/', OrderListCreateView.as_view(), name='orders'),
+    path('orders/<int:pk>/', OrderDetailView.as_view(), name='order-detail'),
+    # path('api-token-auth/', obtain_auth_token, name='api-token'),
+]
---
+++ api/utils.py
@@ -0,0 +1,16 @@
+import africastalking
+from django.dispatch.dispatcher import logger
+
+
+def send_sms_alert(customer_name, order_item, recipient_phone):
+    # initialize sms service
+    sms = africastalking.SMS
+
+    message = 'Hello {customer_name}, your order for {order_item} has been added.'
+    recipients = [recipient_phone]
+
+    try:
+        response = sms.send(message, recipients)
+        print(response)
+    except Exception as e:
+        logger.exception('An error occurred while sending the SMS: {e}')
